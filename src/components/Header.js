34eb3420257dc6e77b20333a2f301a0fae39f6bc
DharaMaharaj04 <maharajdhara95@gmail.com>
2021-10-11 12:47:49 +0530

    header  commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
 function Header() {
     return (
-            <header className="header" role="banner">
+            <header className="header navbar-light" role="banner">
                 <div className="container-fuild" id="head-stuff">
                     <div className="logo">
-                        <a href="/">
+                        <a href="https://kidsville.in">
                             <img src="/assets/img/kidsville logo-2.png" alt="kidsville Dental" className="img-responsive" width="250" height="120" />
                         </a>
                     </div>
@@ -19,86 +18,97 @@ function Header() {
                             <h2 className="widgettitle">Special Offer 1</h2>
                             <div className="textwidget"><strong>Braces/Invisalign Consultations</strong> - Schedule a consultation.<a href="/"><strong>Click Here</strong></a>
                             </div>
-                        </div>		
+                        </div>        
                     </div>
                 </div>
                 <div className="container-fuild">
                     <div className="row">
                         <div className="col-12 col-lg-2">
                             <div className="kidsville-logo">
-                                <a href="/">
+                                <a href="https://kidsville.in">
                                     <img src="/assets/img/kidsville logo-2.png" alt="kidsville Dental" className="img-responsive" width="180" height="100" />
                                 </a>
                             </div>
                         </div>
                         <div className="col-12 col-lg-10">
-                            <div className="navbar">
-                                <div className="dropdown">
-                                    <button className="dropbtn"><a href="/">Home</a></button>
-                                    <div className="dropdown-content">
-                                        <a href="#">Meet Our Team!</a>
-                                        <a href="#">About Us</a>
-                                        <a href="#">Why US?</a>
-                                        <a href="#">Location</a>
+                            <div className="navbar navbar-expand-lg">
+                                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                                </button> 
+                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <div className="dropdown navbar-nav end">
+                                        <button className="dropbtn drop-down"><a className="dropdown-toggle" href="/" data-toggle="dropdown" aria-expanded="false">Home</a>
+                                            <div className="dropdown-content dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <a href="#">Meet Our Team!</a>
+                                                <a href="#">About Us</a>
+                                                <a href="#">Why US?</a>
+                                                <a href="#">Location</a>
+                                            </div>
+                                        </button>
                                     </div>
-                                </div>
-                                <div className="dropdown">
-                                    <button className="dropbtn"><a href="/children-dentistry">Children's Dentistry</a></button>
-                                    <div className="dropdown-content">
-                                        <a href="#">Your First Visit</a>
-                                        <a href="#">Filling</a>
-                                        <a href="#">Root Canal Treatment in children</a>
-                                        <a href="#">Children's Crowns</a>
-                                        <a href="#">Tooth Removal</a>
-                                        <a href="#">Space Maintainer</a>
-                                        <a href="#">Digital X-ray</a>
-                                        <a href="#">Emergency Dental Treatment</a>
+                                    <div className="dropdown navbar-nav end">
+                                        <button className="dropbtn drop-down"><a className="dropdown-toggle" href="/children-dentistry" data-toggle="dropdown" aria-expanded="false">Children's Dentistry</a>
+                                            <div className="dropdown-content dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <a href="#">Your First Visit</a>
+                                                <a href="#">Filling</a>
+                                                <a href="#">Root Canal Treatment in children</a>
+                                                <a href="#">Children's Crowns</a>
+                                                <a href="#">Tooth Removal</a>
+                                                <a href="#">Space Maintainer</a>
+                                                <a href="#">Digital X-ray</a>
+                                                <a href="#">Emergency Dental Treatment</a>
+                                            </div>
+                                        </button>
                                     </div>
-                                </div>
-                                <div className="dropdown">
-                                    <button className="dropbtn"><a href="/preventive-dentistry">Preventive Dentistry</a></button>
-                                    <div className="dropdown-content">
-                                        <a href="#">Dental Hygiene For Children</a>
-                                        <a href="#">Dental Hypomineralization</a>
-                                        <a href="#">Dental Sealants</a>
-                                        <a href="#">Fluoride Varnish</a>
+                                    <div className="dropdown navbar-nav end">
+                                        <button className="dropbtn drop-down"><a className=" dropdown-toggle" href="/preventive-dentistry" data-toggle="dropdown" aria-expanded="false">Preventive Dentistry</a>
+                                            <div className="dropdown-content dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <a href="#">Dental Hygiene For Children</a>
+                                                <a href="#">Dental Hypomineralization</a>
+                                                <a href="#">Dental Sealants</a>
+                                                <a href="#">Fluoride Varnish</a>
+                                            </div>
+                                        </button>
                                     </div>
-                                </div>
-                                <div className="dropdown">
-                                    <button className="dropbtn"><a href="/sedation-services">Sedation Services</a></button>
-                                    <div className="dropdown-content">
-                                        <a href="#">Treatment under IV Sedation</a>
-                                        <a href="#">Treatment Under General Anesthesia</a>
+                                    <div className="dropdown navbar-nav end">
+                                        <button className="dropbtn drop-down"><a className="dropdown-toggle" href="/sedation-services" data-toggle="dropdown" aria-expanded="false">Sedation Services</a>
+                                            <div className="dropdown-content dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <a href="#">Treatment under IV Sedation</a>
+                                                <a href="#">Treatment Under General Anesthesia</a>
+                                            </div>
+                                        </button>
                                     </div>
-                                </div>
-                                <div className="dropdown">
-                                    <button className="dropbtn"><a href="/othodontics">Othodontics</a></button>
-                                    <div className="dropdown-content">
-                                        <a href="#">Braces Consdivt</a>
-                                        <a href="#">Orthodontic Corrections</a>
-                                        <a href="#">Braces</a>
-                                        <a href="#">Invisalign</a>
-                                        <a href="#">Clear Aligners or Braces?</a>
-                                        <a href="#">Early Orthodontics</a>
-                                        <a href="#">Management of Oral Habits</a>
-                                        <a href="#">Orthodontics for Teens</a>
-                                        <a href="#">Myofunctional Treatment</a>
-                                        <a href="#">Retainers</a>
+                                    <div className="dropdown navbar-nav end">
+                                        <button className="dropbtn drop-down"><a className="dropdown-toggle" href="/othodontics" data-toggle="dropdown" aria-expanded="false">Othodontics</a>
+                                            <div className="dropdown-content dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <a href="#">Braces Consdivt</a>
+                                                <a href="#">Orthodontic Corrections</a>
+                                                <a href="#">Braces</a>
+                                                <a href="#">Invisalign</a>
+                                                <a href="#">Clear Aligners or Braces?</a>
+                                                <a href="#">Early Orthodontics</a>
+                                                <a href="#">Management of Oral Habits</a>
+                                                <a href="#">Orthodontics for Teens</a>
+                                                <a href="#">Myofunctional Treatment</a>
+                                                <a href="#">Retainers</a>
+                                            </div>
+                                        </button>
                                     </div>
-                                </div>
-                                <div className="dropdown">
-                                    <button className="contact-menu dropbtn"><a href="/contact-us">Contact US</a></button>
-                                </div> 
-                                <div className="dropdown">
-                                    <button className="dropbtn"><a href="/kidsville-max">Kidsville Max</a></button>
-                                    <div className="dropdown-content">
-                                        <a href="/blogs">Blogs</a>
-                                        <a href="#">Covid 19 Updates</a>
-                                        <a href="#">Newsletter Sign-Up</a>
-                                        <a href='#'>FAQs</a>
+                                    <div className="dropdown navbar-nav end">
+                                        <button className="contact-menu dropbtn"><a href="/contact-us">Contact US</a></button>
+                                    </div> 
+                                    <div className="dropdown navbar-nav end">
+                                        <button className="dropbtn drop-down"><a className="dropdown-toggle" href="/kidsville-max" data-toggle="dropdown" aria-expanded="false">Kidsville Max</a>
+                                            <div className="dropdown-content dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <a href="/blogs">Blogs</a>
+                                                <a href="#">Covid 19 Updates</a>
+                                                <a href="#">Newsletter Sign-Up</a>
+                                                <a href='#'>FAQs</a>
+                                            </div>
+                                        </button>
                                     </div>
+                                    <div className="appointment-btn dropdown navbar-nav end"><a href="/">appointment</a></div>  
                                 </div>
-                                <div className="appointment-btn"><a href="/">appointment</a></div>  
                             </div>
                         </div>
                     </div>
@@ -107,6 +117,4 @@ function Header() {
     )
 }
         
-
-
 export default Header                    
